e67f37f2d9b6ed32172e4978db857f460cadfa18
Ezekielutshudi <ezekielutshudi@gmail.com>
2022-08-18 15:02:10 +0200

    add the doctors view and export doctor view as default

--- src/features/doctor/DoctorView.jsx
+++ src/features/doctor/DoctorView.jsx
@@ -14,3 +14,28 @@ const DoctorView = () => {
     if (loading) {
       return <Loader />;
     }
+
+    return (
+        <div className="doctorView-container">
+          <div className="doctorView-image-container">
+            <img
+              src={doctor.photo}
+              alt={doctor.name}
+              className="doctorView-image"
+            />
+          </div>
+          <div className="doctorView-info">
+            <h4>{doctor.name}</h4>
+            <p>{doctor.speciality}</p>
+            <p>Price: ${doctor.price}</p>
+            <h6 style={{ textAlign: 'left', fontWeight: 'bold' }}>Bio</h6>
+            <p style={{ textAlign: 'left' }}>{doctor.bio}</p>
+            <Link to="/new_appointment" className="doctorView-reserve">
+              Book appointment
+            </Link>
+          </div>
+        </div>
+      );
+    };
+    
+    export default DoctorView;
