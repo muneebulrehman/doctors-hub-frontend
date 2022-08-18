a8c8ae7f266d3f9d86ab8f77ff88795bd7609b92
Ezekielutshudi <ezekielutshudi@gmail.com>
2022-08-18 15:01:15 +0200

    add the doctors view

--- src/features/doctor/DoctorView.jsx
+++ src/features/doctor/DoctorView.jsx
@@ -2,6 +2,15 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
 
-import { fetchSingleDoctor } from './doctorSlice';
-import './doctors.css';
-import Loader from '../loader/Loader';
+const DoctorView = () => {
+    const doctor = useSelector((state) => state.doctor.doctor);
+    const loading = useSelector((state) => state.doctor.loading);
+    const dispatch = useDispatch();
+    const { doctorId } = useParams();
+    useEffect(() => {
+      dispatch(fetchSingleDoctor(doctorId));
+    }, []);
+  
+    if (loading) {
+      return <Loader />;
+    }
