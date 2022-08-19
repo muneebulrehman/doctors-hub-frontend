426073820286cbf4b85bec34ada2b10e39622880
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-08-19 22:40:03 +0530

    Added sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { NavLinks } from 'react-router-dom';
-import { AiOutlineClose } from 'react-icons/ai';
+import { NavLink } from 'react-router-dom';
+// import { AiOutlineClose } from 'react-icons/ai';
+import { BiLeftArrow } from 'react-icons/bi';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { ImTwitter, ImFacebook, ImGooglePlus, ImVimeo, ImPinterest } from 'react-icons/im';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
@@ -11,20 +13,49 @@ const Navbar = () => {
     setMenu(!menu);
   };
 
+  if (menu) {
+    return (
+      <p className={styles.hamburger} onClick={() => toggleMenu()}>
+        <GiHamburgerMenu />
+      </p>
+    );
+  }
+
   return (
     <div className={styles.sidebar}>
       <div className={styles.logo}>
         <p>Doctor&apos;s hub</p>
-        {/* <p>Doctors hub</p> */}
-        {menu ? (
-          <p className={styles.closeBtn} onClick={() => toggleMenu()}>
-            <AiOutlineClose />
-          </p>
-        ) : (
-          <p className={styles.hamburger} onClick={() => toggleMenu()}>
-            <GiHamburgerMenu />
-          </p>
-        )}
+        <p className={styles.closeBtn} onClick={() => toggleMenu()}>
+          <BiLeftArrow />
+        </p>
+      </div>
+      <div className={styles.navlinks}>
+        <NavLink to="/">Doctors</NavLink>
+        <NavLink to="/appointments">Appointments</NavLink>
+        <NavLink to="/create-appointment">Reserve appointment</NavLink>
+      </div>
+      <div className={styles.footer}>
+        <button className={styles.logoutbtn}>Logout</button>
+        <div className={styles.icons}>
+          <span>
+            <ImTwitter />
+          </span>
+          <span>
+            <ImFacebook />
+          </span>
+          <span>
+            <ImGooglePlus />
+          </span>
+          <span>
+            <ImVimeo />
+          </span>
+          <span>
+            <ImPinterest />
+          </span>
+        </div>
+        <div className={styles.footerText}>
+          <p>&copy 2022 Max hospitals group </p>
+        </div>
       </div>
     </div>
   );
