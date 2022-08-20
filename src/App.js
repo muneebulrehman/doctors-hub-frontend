0f73bcb30d114a0b13fe44523884bf1d2226b6bf
Ezekielutshudi <ezekielutshudi@gmail.com>
2022-08-20 15:18:17 +0200

    update the app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import './App.css';
-
-import AllDoctorView from './features/doctor/AllDoctorView';
-import DoctorView from './features/doctor/DoctorView';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Routes>
-        <Route path={routesApp.DOCTORS} element={<AllDoctorView />} />
-        <Route path={routesApp.DOCTOR} element={<DoctorView />} />
-      </Routes>
+      <Navbar />
     </div>
   );
 }
