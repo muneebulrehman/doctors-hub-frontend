0adbf196a3556c850dece49b79dd2de1e3884d7f
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-08-20 19:13:53 +0530

    Revert "7 2pt setup doctors reducer"

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import userReducer from '../features/user/userSlice';
-import doctorReducer from '../features/doctor/doctorSlice';
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
-    doctor: doctorReducer
+    user: userReducer
   }
 });
 
