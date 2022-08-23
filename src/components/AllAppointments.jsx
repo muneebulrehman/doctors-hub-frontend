89a7322fb531e1e6a3f7b1af64804396df22213d
Didier Sangwa <50297713+sangwa7@users.noreply.github.com>
2022-08-23 17:06:11 +0200

    Finilize the AllAppointments

--- src/components/AllAppointments.jsx
+++ src/components/AllAppointments.jsx
@@ -19,3 +19,20 @@ const AllAppointments = () => {
         dispatch(userAppointments(userId));
       }
     }, [userId]);
+
+    return (
+        <div>
+          <h2>Booked Appointments</h2>
+          {appointments.map((appointment) => (
+            <div key={appointment.id}>
+              <img src={appointment.doctor.photo} alt={`doctor${appointment.id}`} />
+              <h3>{appointment.doctor.name}</h3>
+              <p>{appointment.date.split('T')[0]}</p>
+            </div>
+          ))}
+        </div>
+      );
+    };
+    
+    export default AllAppointments;
+    
