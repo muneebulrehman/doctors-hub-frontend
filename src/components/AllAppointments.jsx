ca0f305c571baf7e570b56bc220435838430f8be
Didier Sangwa <50297713+sangwa7@users.noreply.github.com>
2022-08-23 17:05:27 +0200

    Add AllAppointments Function

--- src/components/AllAppointments.jsx
+++ src/components/AllAppointments.jsx
@@ -1,3 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { userAppointments } from '../features/appointment/appointmentSlice';
+
+const AllAppointments = () => {
+    const dispatch = useDispatch();
+    const appointments = useSelector((state) => state.appointment.appointments);
+    console.log(appointments);
+    const [userId, setUserId] = useState();
+  
+    useEffect(() => {
+      if (localStorage.getItem('user_id')) {
+        setUserId(+localStorage.getItem('user_id'));
+      }
+    }, []);
+  
+    useEffect(() => {
+      if (userId) {
+        dispatch(userAppointments(userId));
+      }
+    }, [userId]);
